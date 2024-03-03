3e4672aa6042e2dfccf7b5a06d6cddd61b26d6ad
Roman Smolyakov <123468411+Roman9456@users.noreply.github.com>
2024-03-03 15:24:38 +0300

    Update task.js

--- JS_HW-6/popup/task.js
+++ JS_HW-6/popup/task.js
@@ -2,11 +2,32 @@ var modal = document.querySelector('#subscribe-modal');
 
 var closeButton = modal.querySelector('.modal__close');
 
-if (!localStorage.getItem('modalClosed')) {
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+if (!getCookie('modalClosed')) {
     modal.classList.add('modal_active');
 }
 
 closeButton.addEventListener('click', function() {
     modal.classList.remove('modal_active');
-    localStorage.setItem('modalClosed', true);
+    setCookie('modalClosed', true, 365);
 });
